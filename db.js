52384fa51aed89a918db25129fd3968881583a0a
sodra6 <sodra6@naver.com>
2023-02-28 10:10:27 +0900

    db setting

--- db.js
+++ db.js
@@ -16,11 +16,12 @@ pg.connect(err => {
 
 //쿼리 사용시
 const selectUser = (req, res) => {
-  //res.send("Success Here");
-  pg.query("select * from PG_SHADOW", (err, dbRes) => {
+  console.log(req.params.Id);
+  pg.query(`select * from PG_SHADOW where usename = '${req.params.Id}'`, (err, dbRes) => {
     console.log(err, dbRes.rows);
     if (err != null) console.log(err);
-    res.send({ data: dbRes.rows });
+    res.send("Success Here");
+    res.send({ data: dbRes.rows.usename });
   });
 };
 
