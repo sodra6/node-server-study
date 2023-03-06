8d9005d4ad06bdef84a64d294d945372167ef869
sodra6 <sodra6@naver.com>
2023-03-06 15:43:55 +0900

    commit port modify

--- server.js
+++ server.js
@@ -7,14 +7,19 @@ const api = require("./db.js");
 app.use(cors());
 app.use(express.json());
 
-app.listen(8080, () => {
-  console.log("Listening at 8080");
+app.listen(3000, () => {
+  console.log("Listening at 3000");
 });
 
-app.get("/getProjectList", (req, res) => {
-  console.log("getProjectList");
-  api.pg.query("select t1.prjct_nm, t2.* from project_list t1, project_info t2 where t1.prjct_id = t2.prjct_id", (err, result) => {
-    if (err) res.sendStatus(500);
-    else res.status(200).json(result.rows);
+app
+  .route("/projectList")
+  .get((req, res) => {
+    console.log("getProjectList");
+    api.pg.query("select t1.prjct_nm, t2.* from project_list t1, project_info t2 where t1.prjct_id = t2.prjct_id", (err, result) => {
+      if (err) res.sendStatus(500);
+      else res.status(200).json(result.rows);
+    });
+  })
+  .post((req, res) => {
+    res.send("add project");
   });
-});
