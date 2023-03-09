e7ee20bf12a0980d3682b25d7fa11bfa48ebe3d9
sodra6 <sodra6@naver.com>
2023-03-09 14:32:04 +0900

    get menu, company data

--- server.js
+++ server.js
@@ -19,7 +19,6 @@ app.listen(3000, () => {
 app
   .route("/api/projectList")
   .get((req, res) => {
-    console.log("get method");
     api.pg.query("select t1.prjct_nm, t2.* from project_list t1, project_info t2 where t1.prjct_id = t2.prjct_id", (err, result) => {
       if (err) {
         console.log("query err");
@@ -28,11 +27,42 @@ app
     });
   })
   .post((req, res) => {
-    console.log("post method");
     api.pg.query("select t1.prjct_nm, t2.* from project_list t1, project_info t2 where t1.prjct_id = t2.prjct_id", (err, result) => {
       if (err) {
         console.log("query err");
         res.sendStatus(500);
       } else res.status(200).json(result.rows);
+    });
+  });
+
+app
+  .route("/api/companyList")
+  .get((req, res) => {
+    api.pg.query("select * from company", (err, result) => {
+      if (err) {
+        console.log("query err");
+        res.sendStatus(500);
+      } else res.status(200).json(result.rows);
+    });
+  })
+  .post((req, res) => {
+    api.pg.query("select t1.prjct_nm, t2.* from project_list t1, project_info t2 where t1.prjct_id = t2.prjct_id", (err, result) => {
+      console.log("post");
+    });
+  });
+
+app
+  .route("/api/menuList")
+  .get((req, res) => {
+    api.pg.query("select * from mnu_list", (err, result) => {
+      if (err) {
+        console.log("query err");
+        res.sendStatus(500);
+      } else res.status(200).json(result.rows);
+    });
+  })
+  .post((req, res) => {
+    api.pg.query("select t1.prjct_nm, t2.* from project_list t1, project_info t2 where t1.prjct_id = t2.prjct_id", (err, result) => {
+      console.log("post");
     });
   });
